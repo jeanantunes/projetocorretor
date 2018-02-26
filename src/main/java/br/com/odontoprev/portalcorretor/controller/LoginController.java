184b8ffce17f516c3a8e75f1a8b7f47e9256e37f
odp108311 <odp108311@APHTECL108311.odontoprev.com.br>
2018-02-26 19:16:50 -0300

    Template Resolver

--- src/main/java/br/com/odontoprev/portalcorretor/controller/LoginController.java
+++ src/main/java/br/com/odontoprev/portalcorretor/controller/LoginController.java
@@ -1,13 +1,23 @@
 package br.com.odontoprev.portalcorretor.controller;
 
-import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class LoginController {
 
-    @RequestMapping("/index")
-    public String Index( Model login) {
-        return "index";
+    @RequestMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("index");
+    }
+    @RequestMapping(value="/autenticar",method= RequestMethod.POST)
+    public String autenticar(@ModelAttribute("login") br.com.odontoprev.portalcorretor.model.login login) {
+      	System.out.println(login.getLogin());
+    	System.out.println(login.getSenha());
+    	return "autenticado";
     }
 
 }
