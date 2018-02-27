60d1957b4022ef85df9eb3ce8a193212a1e73c18
Leandro Jacomelli <leandro.jacomelli@gmail.com>
2018-02-27 20:53:37 -0300

    merge

---
+++ src/main/java/br/com/odontoprev/portalcorretor/controller/LoginController.java
@@ -0,0 +1,24 @@
+package br.com.odontoprev.portalcorretor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/")
+    public String index(Principal principal) {
+        return "".equals(principal.getName()) ? "login" : principal.getName().length() == 11 ? "corretor/homeCorretor" : "corretor/homeCorretora";
+    }
+/*
+    @RequestMapping(value = "/autenticar", method = RequestMethod.POST)
+    public ModelAndView autenticar(@ModelAttribute("login") br.com.odontoprev.portalcorretor.model.Login login) {
+        System.out.println(login.getLogin());
+        System.out.println(login.getSenha());
+        return new ModelAndView("/corretor/vendas_pme/home");
+    }
+*/
+
+}
