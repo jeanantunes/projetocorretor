944f273ec8042d24c7e88bfb824ab254595a9be5
Berlezi <berlezi@gmail.com>
2018-02-27 23:25:41 -0300

    model dashboard pf

--- src/main/java/br/com/odontoprev/portalcorretor/model/DashboardCorretora.java
+++ src/main/java/br/com/odontoprev/portalcorretor/model/DashboardCorretora.java
@@ -1,4 +1,11 @@
 package br.com.odontoprev.portalcorretor.model;
 
 public class DashboardCorretora {
+
+    private int countCorretoresAprovacao;
+    private int countCorretoresCriticadas;
+
+    private Double valorPessoaFisica;
+    private Double valorPME;
+
 }
