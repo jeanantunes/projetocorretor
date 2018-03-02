d97e285c57a244c041d3235fe3d7fc0c7571061c
Berlezi <berlezi@gmail.com>
2018-03-02 00:52:33 -0300

    removido fonte de referencia do app

--- Fonte_APP/src_APP/main/assets/js/odontoprevstick.js
+++
@@ -1,9 +0,0 @@
-// Slick //
-$(document).on('ready', function() {
-			
-    $('.single-item').slick({
-        lazyLoad: 'ondemand', // ondemand progressive anticipated
-        infinite: true,
-        dots: true
-    });
-});
