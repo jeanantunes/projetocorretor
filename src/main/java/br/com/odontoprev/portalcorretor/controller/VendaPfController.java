cd828aeaf980ae5f7ff853892cfb552a56333e8a
Igor Rotondo Bagliotti <igor.bagliotti@gmail.com>
2018-03-02 16:26:51 -0300

    Adicionando mock de objetos vendaPF

---
+++ src/main/java/br/com/odontoprev/portalcorretor/controller/VendaPfController.java
@@ -0,0 +1,79 @@
+package br.com.odontoprev.portalcorretor.controller;
+
+import br.com.odontoprev.portalcorretor.model.Cadastro;
+import br.com.odontoprev.portalcorretor.model.VendaPf;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class VendaPfController {
+
+    @RequestMapping(value = "/venda/pf", method = RequestMethod.GET)
+    public ModelAndView home() {
+        
+    	VendaPf vendaPf = new VendaPf();
+    	vendaPf.setCelularTitularPlano("(11) 11111-1111");
+    	vendaPf.setCepTitularPlano("15900-000");
+    	vendaPf.setCpfTitularPlano("000.000.000-11");
+    	vendaPf.setDataNascimentoTitularPlano("03/02/1993");
+    	vendaPf.setEmailTitularPlano("teste@teste.com.br");
+    	vendaPf.setEnderecoTitularPlano("Rua Siqueira Campos");
+    	vendaPf.setNomeMaeTitularPlano("Cristina da Silva");
+    	vendaPf.setNomeTitularPlano("Marco Da Silva");
+    	vendaPf.setNumeroTitularPlano("12312313");
+    	vendaPf.setSexo("male");
+    	
+        return new ModelAndView("/venda/pf/titularDoPlanoPF", "vendaPf", vendaPf);
+    }
+
+
+    @RequestMapping(value = "/cnpj", method = RequestMethod.GET)
+    public ResponseEntity<Cadastro> cnpj(@RequestParam("cnpj") String cnpj) {
+
+        Cadastro cadastro = new Cadastro();
+        cadastro.setCnpj(cnpj);
+        cadastro.setRazaoSocial("OdontoPrev");
+        cadastro.setCnae("123-12");
+        cadastro.setSimplesNacional("Não");
+        cadastro.setDataAbertura("01/01/2001");
+        cadastro.setStatusCnpj("VÁLIDO");
+        cadastro.setNomeRepresentanteLegal("Representante Legal");
+        cadastro.setCpfRepresentanteLegal("123.123.123-34");
+        cadastro.setTelefone("(11)2222-1234");
+        cadastro.setCelular("(11)98765-1234");
+        cadastro.setEmail("teste@odontoprev.com.br");
+        return cnpj.equals("23.423.423/423") ? ResponseEntity.ok(cadastro) : ResponseEntity.notFound().build();
+    }
+
+    @RequestMapping(value = "/cep", method = RequestMethod.GET)
+    public ModelAndView cep(@ModelAttribute("cadastro") Cadastro cadastro) {
+
+
+        return new ModelAndView("cadastro/home", "cadastro", cadastro);
+    }
+
+
+    @RequestMapping(value = "/cadastrar", method = RequestMethod.POST)
+    public ModelAndView cadastrar(@ModelAttribute("cadastro") Cadastro cadastro) {
+        return new ModelAndView("cadastro/termosCondicoes", "cadastro", cadastro);
+    }
+
+
+    @RequestMapping(value = "/termosCondicoes", method = RequestMethod.POST)
+    public ModelAndView termosCondicoes(@ModelAttribute("cadastro") Cadastro cadastro) {
+        return new ModelAndView("cadastro/bemvindo", "cadastro", cadastro);
+    }
+
+    @RequestMapping(value = "/bemvindo", method = RequestMethod.POST)
+    public ModelAndView bemvindo(@ModelAttribute("cadastro") Cadastro cadastro) {
+        return new ModelAndView("cadastro/bemvindo", "cadastro", cadastro);
+    }
+
+
+}
---
+++ src/main/java/br/com/odontoprev/portalcorretor/model/VendaPf.java
@@ -0,0 +1,78 @@
+package br.com.odontoprev.portalcorretor.model;
+
+public class VendaPf {
+
+	private String nomeTitularPlano;
+	private String cpfTitularPlano;
+	private String sexo;
+	private String dataNascimentoTitularPlano;
+	private String nomeMaeTitularPlano;
+	private String celularTitularPlano;
+	private String emailTitularPlano;
+	private String cepTitularPlano;
+	private String enderecoTitularPlano;
+	private String numeroTitularPlano;
+	public String getNomeTitularPlano() {
+		return nomeTitularPlano;
+	}
+	public void setNomeTitularPlano(String nomeTitularPlano) {
+		this.nomeTitularPlano = nomeTitularPlano;
+	}
+	public String getCpfTitularPlano() {
+		return cpfTitularPlano;
+	}
+	public void setCpfTitularPlano(String cpfTitularPlano) {
+		this.cpfTitularPlano = cpfTitularPlano;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getDataNascimentoTitularPlano() {
+		return dataNascimentoTitularPlano;
+	}
+	public void setDataNascimentoTitularPlano(String dataNascimentoTitularPlano) {
+		this.dataNascimentoTitularPlano = dataNascimentoTitularPlano;
+	}
+	public String getNomeMaeTitularPlano() {
+		return nomeMaeTitularPlano;
+	}
+	public void setNomeMaeTitularPlano(String nomeMaeTitularPlano) {
+		this.nomeMaeTitularPlano = nomeMaeTitularPlano;
+	}
+	public String getCelularTitularPlano() {
+		return celularTitularPlano;
+	}
+	public void setCelularTitularPlano(String celularTitularPlano) {
+		this.celularTitularPlano = celularTitularPlano;
+	}
+	public String getEmailTitularPlano() {
+		return emailTitularPlano;
+	}
+	public void setEmailTitularPlano(String emailTitularPlano) {
+		this.emailTitularPlano = emailTitularPlano;
+	}
+	public String getCepTitularPlano() {
+		return cepTitularPlano;
+	}
+	public void setCepTitularPlano(String cepTitularPlano) {
+		this.cepTitularPlano = cepTitularPlano;
+	}
+	public String getEnderecoTitularPlano() {
+		return enderecoTitularPlano;
+	}
+	public void setEnderecoTitularPlano(String enderecoTitularPlano) {
+		this.enderecoTitularPlano = enderecoTitularPlano;
+	}
+	public String getNumeroTitularPlano() {
+		return numeroTitularPlano;
+	}
+	public void setNumeroTitularPlano(String numeroTitularPlano) {
+		this.numeroTitularPlano = numeroTitularPlano;
+	}
+
+	
+
+}
