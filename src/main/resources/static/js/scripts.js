9420902af86462c42fdd6e061ed8c0baa3f127a6
Jean Antunes <jean.antunes@gmail.com>
2018-04-25 12:35:33 -0300

    Portal - Random Image DONE

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,1 +1,5 @@
-// Empty JS for your own code to be here
+// Random background login.html
+$(document).ready(function() {
+    var images = ['portal.jpg', 'portal_02.jpg', 'portal_03.jpg', 'portal_04.jpg'];
+    $('.container-cadastro').css({'background-image': 'url(../img/' + images[Math.floor(Math.random() * images.length)] + ')'});
+});
