008edb0879a76e3c9524a5fc3ca7f18fb494b574
Jean Antunes <feodpv01048@odontoprev.com.br>
2018-05-02 19:05:25 -0300

    Upload e Download

--- src/main/java/br/com/odontoprev/portalcorretor/controller/CorretoraController.java
+++ src/main/java/br/com/odontoprev/portalcorretor/controller/CorretoraController.java
@@ -1,5 +1,23 @@
 package br.com.odontoprev.portalcorretor.controller;
 
+import br.com.odontoprev.portalcorretor.model.ListaPropostas;
+import br.com.odontoprev.portalcorretor.model.UsuarioSession;
+import br.com.odontoprev.portalcorretor.service.CorretoraService;
+import br.com.odontoprev.portalcorretor.service.DashService;
+import br.com.odontoprev.portalcorretor.service.EnderecoService;
+import br.com.odontoprev.portalcorretor.service.ForcaVendaService;
+import br.com.odontoprev.portalcorretor.service.dto.*;
+import br.com.odontoprev.portalcorretor.service.entity.FiltroStatusProposta;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Date;
@@ -7,58 +25,34 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import javax.servlet.http.HttpSession;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-import br.com.odontoprev.portalcorretor.model.ListaPropostas;
-import br.com.odontoprev.portalcorretor.model.UsuarioSession;
-import br.com.odontoprev.portalcorretor.service.CorretoraService;
-import br.com.odontoprev.portalcorretor.service.DashService;
-import br.com.odontoprev.portalcorretor.service.EnderecoService;
-import br.com.odontoprev.portalcorretor.service.ForcaVendaService;
-import br.com.odontoprev.portalcorretor.service.dto.Corretora;
-import br.com.odontoprev.portalcorretor.service.dto.DashResponse;
-import br.com.odontoprev.portalcorretor.service.dto.DashboardPropostas;
-import br.com.odontoprev.portalcorretor.service.dto.ForcaVenda;
-import br.com.odontoprev.portalcorretor.service.dto.Proposta;
-import br.com.odontoprev.portalcorretor.service.entity.FiltroStatusProposta;
-
 @Controller
 public class CorretoraController {
-	
-	private static final Log log = LogFactory.getLog(CorretoraController.class);
+
+    private static final Log log = LogFactory.getLog(CorretoraController.class);
 
     @Autowired
     DashService dashService;
 
     @Autowired
     ForcaVendaService forcaVendaService;
-    
+
     @Autowired
     CorretoraService corretoraService;
-    
+
     @Autowired
     EnderecoService enderecoService;
 
 
     @RequestMapping(value = "corretora/home", method = RequestMethod.GET)
-    public ModelAndView home(HttpSession session) {    	
-    	final long start = new Date().getTime();
-    	log.info("CorretoraController.home - inicio");
+    public ModelAndView home(HttpSession session) {
+        final long start = new Date().getTime();
+        log.info("CorretoraController.home - inicio");
         UsuarioSession usuario = (UsuarioSession) session.getAttribute("usuario");
 
         //TODO retornar valores nas propostas
         DashboardPropostas propostaPME = dashService.ObterListaPropostaPME(FiltroStatusProposta.TODOS, usuario.getDocumento());
         DashboardPropostas propostaPF = dashService.ObterListaPropostaPF(FiltroStatusProposta.TODOS, usuario.getDocumento());
-        DashResponse[] dashs = dashService.obterPorDocumento(null,null,usuario.getDocumento());        
+        DashResponse[] dashs = dashService.obterPorDocumento(null, null, usuario.getDocumento());
 
         ListaPropostas corretora = new ListaPropostas();
         List<Proposta> propostasPME = propostaPME.getDashboardPropostasPME();
@@ -74,19 +68,19 @@ public class CorretoraController {
         corretora.setCountCorretoresAprovacao(aguardando.intValue());
 
         List<Proposta> dashsConverter = Arrays.asList(dashs).stream()
-        		.map(
-        		dash -> new Proposta(
-	        				dash.getNomeForca(),
-	        				dash.getTipoVenda(),
-	        				new Date(),
-	        				dash.getValorTotal()
-	        				)
-        		)
-        		.collect(Collectors.toList());
-        
-        Stream<Proposta> propostas = Stream.concat(propostasPF.stream().peek(i -> i.setTipoPlano("PF")), propostasPME.stream().peek(i -> i.setTipoPlano("PME")));	                
-        	
-        
+                .map(
+                        dash -> new Proposta(
+                                dash.getNomeForca(),
+                                dash.getTipoVenda(),
+                                dash.getDtVenda().replaceAll("-", "/"),
+                                dash.getValorTotal()
+                        )
+                )
+                .collect(Collectors.toList());
+
+        Stream<Proposta> propostas = Stream.concat(propostasPF.stream().peek(i -> i.setTipoPlano("PF")), propostasPME.stream().peek(i -> i.setTipoPlano("PME")));
+
+
         corretora.setPropostas(dashsConverter);
         Long aprovada = propostas.filter(p -> p.getStatusVenda().equals("Aprovado")).count();
         Long criticadas = propostasPF.size() + propostasPME.size() - aprovada;
@@ -96,9 +90,9 @@ public class CorretoraController {
 
         corretora.setTotalSucesso(aprovada.intValue());
         corretora.setTotalCriticadas(criticadas.intValue());
-        
-      //  double totalValorPF = propostaPF.getDashboardPropostasPF().stream().mapToDouble(Proposta::getValor).sum();
-      //  double totalValorPME = propostaPME.getDashboardPropostasPME().stream().mapToDouble(Proposta::getValor).sum();
+
+        //  double totalValorPF = propostaPF.getDashboardPropostasPF().stream().mapToDouble(Proposta::getValor).sum();
+        //  double totalValorPME = propostaPME.getDashboardPropostasPME().stream().mapToDouble(Proposta::getValor).sum();
         double totalValorPF = dashsConverter.stream().filter(p -> p.getTipoPlano().equals("PF")).mapToDouble(Proposta::getValor).sum();
         double totalValorPME = dashsConverter.stream().filter(p -> p.getTipoPlano().equals("PME")).mapToDouble(Proposta::getValor).sum();
 
@@ -109,20 +103,20 @@ public class CorretoraController {
         corretora.setPercenteValorPME(totalValorPME > totalValorPF ? 100 : totalValorPME == 0 ? 0 : 50);
 
         long time = new Date().getTime() - start;
-        log.info("CorretoraController.home - fim - "+ time+"ms");
+        log.info("CorretoraController.home - fim - " + time + "ms");
         return new ModelAndView("corretora/home", "corretora", corretora);
     }
-   
-	@RequestMapping(value = "corretora/cadastro/editar", method = RequestMethod.GET)
+
+    @RequestMapping(value = "corretora/cadastro/editar", method = RequestMethod.GET)
     public ModelAndView Editar(HttpSession session) {
 
-    	UsuarioSession usuario = (UsuarioSession) session.getAttribute("usuario");
+        UsuarioSession usuario = (UsuarioSession) session.getAttribute("usuario");
 
-    	Corretora corretora = corretoraService.ObterDadosCorretora(usuario.getDocumento());
-    	
-    	return new ModelAndView("corretora/cadastro/editar", "corretora", corretora);
+        Corretora corretora = corretoraService.ObterDadosCorretora(usuario.getDocumento());
+
+        return new ModelAndView("corretora/cadastro/editar", "corretora", corretora);
     }
-  
+
     @RequestMapping(value = "lista-propostas/{statusProposta}", method = RequestMethod.GET)
     public ModelAndView Proposta(@PathVariable String statusProposta, HttpSession session) {
 
