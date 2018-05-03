f16a09249ca7725388aefc64db578cdf4f8f8ded
Jean Antunes <feodpv01048@odontoprev.com.br>
2018-05-03 19:36:07 -0300

    Home equipe

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -58,7 +58,7 @@ $(document).ready(function () {
     }
     // This must be a hyperlink
     $("#export").click(function (event) {
-        var outputFile = 'relatorio-gestao-vendas'
+        var outputFile = 'template'
         //var outputFile = window.prompt("What do you want to name your output file (Note: This won't have any effect on Safari)") || 'export';
         outputFile = outputFile.replace('.csv','') + '.csv'
 
