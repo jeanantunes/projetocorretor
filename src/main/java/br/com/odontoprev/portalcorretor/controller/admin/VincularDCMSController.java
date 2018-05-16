9580410ce4567dcc3e61e554729172fcf592b688
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-05-16 12:16:25 -0300

    COR-170 60% (esert)

--- src/main/java/br/com/odontoprev/portalcorretor/controller/admin/VincularDCMSController.java
+++ src/main/java/br/com/odontoprev/portalcorretor/controller/admin/VincularDCMSController.java
@@ -1,68 +1,54 @@
 package br.com.odontoprev.portalcorretor.controller.admin;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.odontoprev.portalcorretor.model.VincularDCMSModel;
+import br.com.odontoprev.portalcorretor.service.EmpresaService;
+import br.com.odontoprev.portalcorretor.service.dto.CnpjDadosDCMSResponse;
 
 @Controller
 public class VincularDCMSController {
-    @RequestMapping("admin/update_dcms")
-    public String indexInfoPlano() {
+	
+	@Autowired
+    private EmpresaService empresaService;
+
+	@RequestMapping("admin/update_dcms")
+    public String index() {
         return "admin/update_dcms";
-
     }
 
-    /*
-    @RequestMapping("/info-planos-master-le")
-    public String indexInfoPlanoMasterLe() {
-        return "info-planos-master-le";
+	@RequestMapping(value = "buscarCnpj", method = RequestMethod.POST)
+	public ModelAndView buscarCnpj(HttpSession session, @ModelAttribute("vincularDCMS") VincularDCMSModel vincularDCMSModel) {
+	
+//		UsuarioSession usuario = (UsuarioSession) session.getAttribute("usuario");
+	    
+	    String cnpj = "11431155000107";
+	    
+	    vincularDCMSModel.setCnpj(cnpj); //qg debug
+	    
+	    if (vincularDCMSModel.getCnpj() == null || "".equals(vincularDCMSModel.getCnpj())) {
+	    	vincularDCMSModel.setObservacao("O campo CNPJ é obrigatório");
+	        return new ModelAndView("update_dcms", "vincularDCMS", vincularDCMSModel);
+	    }
+	    
+	    cnpj = vincularDCMSModel.getCnpj().replace(".","").replace("/","").replace("-","");
+	    
+	    CnpjDadosDCMSResponse cnpjDadosDCMSResponse = empresaService.obterDadosEmpresaDCMS(cnpj);
+	
+	    vincularDCMSModel.setCnpj(cnpjDadosDCMSResponse.getCnpj());
+	    vincularDCMSModel.setCdEmpresa(cnpjDadosDCMSResponse.getCdEmpresa());
+	    vincularDCMSModel.setEmpDcms(cnpjDadosDCMSResponse.getEmpDcms());
+	    vincularDCMSModel.setObservacao(cnpjDadosDCMSResponse.getObservacao());
+	
+	    return new ModelAndView("admin/update_dcms", "vincularDCMS", vincularDCMSModel);
+	
+	}
 
-    }
-
-    @RequestMapping("/info-planos-rol-min")
-    public String indexInfoPlanoRolMin() {
-        return "info-planos-rol-min";
-
-    }
-
-
-    @RequestMapping("/info-planos-dental-bem-estar")
-    public String indexInfoPlanoDentalBemEstar() {
-        return "info-planos-dental-bem-estar";
-
-    }
-
-    @RequestMapping("/info-planos-dental-vip")
-    public String indexInfoPlanoDentalVip() {
-        return "info-planos-dental-vip";
-
-    }
-    @RequestMapping("/info-planos-dental-orto")
-    public String indexInfoPlanoDentalOrto() {
-        return "info-planos-dental-orto";
-
-    }
-
-    @RequestMapping("/info-planos-dental-estetica")
-    public String indexInfoPlanoDentalEstetica() {
-        return "info-planos-dental-estetica";
-
-    }
-    
-    @RequestMapping("/info-planos-PF")
-    public String indexInfoPlanoPF() {
-        return "info-planos-PF";
-
-    }
-    
-    @RequestMapping("/info-planos-PME")
-    public String indexInfoPlanoPME() {
-        return "info-planos-PME";
-
-    }
-    
-    @RequestMapping("/email")
-    public String email() {
-        return "email";
-    }
-    */
 }
--- src/main/java/br/com/odontoprev/portalcorretor/service/EmpresaService.java
+++ src/main/java/br/com/odontoprev/portalcorretor/service/EmpresaService.java
@@ -1,11 +1,17 @@
 package br.com.odontoprev.portalcorretor.service;
 
-import br.com.odontoprev.portalcorretor.model.CnpjDadosAceiteResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import br.com.odontoprev.portalcorretor.model.CnpjDadosAceiteResponse;
+import br.com.odontoprev.portalcorretor.service.dto.CnpjDadosDCMSResponse;
 
 @Service
 public class EmpresaService {
@@ -36,6 +42,33 @@ public class EmpresaService {
 
                 cnpjDadosAceiteResponse = retorno.getBody();
                 return cnpjDadosAceiteResponse;
+            } else {
+                return null;
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+  
+    //201805161145 - esert - COR-170
+    public CnpjDadosDCMSResponse obterDadosEmpresaDCMS(String cnpj) {
+        CnpjDadosDCMSResponse cnpjDadosDCMSResponse = null;
+
+        String url = requesBasetUrl + dadosEmpresa + cnpj;
+        RestTemplate restTemplate = new RestTemplate();
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.set("Authorization", "Bearer " + apiManagerTokenService.getToken());
+            HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
+            ResponseEntity<CnpjDadosDCMSResponse> retorno = restTemplate.exchange(url, HttpMethod.GET, entity, CnpjDadosDCMSResponse.class);
+
+            if (retorno.getStatusCode() == HttpStatus.OK) {
+
+                cnpjDadosDCMSResponse = retorno.getBody();
+                return cnpjDadosDCMSResponse;
             } else {
                 return null;
             }
