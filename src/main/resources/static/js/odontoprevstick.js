6de5776e0318279d05fb1b5207b61b20073fcc00
Yago Almeida <yalmeida@vectoritcgroup.com>
2018-06-14 13:11:45 -0300

    Add btn de ver senha, corrigido bug dos cards pf e pme, e criado modal com data de vigencia

--- src/main/resources/static/js/odontoprevstick.js
+++ src/main/resources/static/js/odontoprevstick.js
@@ -1,9 +1,29 @@
 // Slick //
 $(document).on('ready', function() {
 			
+
     $('.single-item').slick({
         lazyLoad: 'ondemand', // ondemand progressive anticipated
         infinite: true,
-        dots: true
+        dots: true,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                autoPlay: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }
+        ]
     });
 });
