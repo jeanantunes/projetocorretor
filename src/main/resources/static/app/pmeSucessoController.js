59528074ae59bafe7d250ec9f22ff061b6035c7a
Yago Almeida Santos <yalmeida@vectoritcgroup.com>
2018-08-27 17:46:05 -0300

    COR-623 Consumindo servico de PDF e rota de properties

--- src/main/resources/static/app/pmeSucessoController.js
+++ src/main/resources/static/app/pmeSucessoController.js
@@ -1,24 +1,121 @@
-﻿var preenchidos = false;
+﻿var URLBase = "";
+var contexto = "";
+var metodo = "";
 
 $(document).ready(function () {
 
     localStorage.removeItem("proposta");
 
+    getPropertie(function (dataUrlBase) {
+
+        URLBase = dataUrlBase;
+
+        getPropertie(function (dataContexto) {
+
+            contexto = dataContexto;
+
+            getPropertie(function (dataMetodo) {
+
+                metodo = dataMetodo;
+
+            },"odontoprev.arquivocontratacao");
+
+        }, "odontoprev.corretor.api.contexto.api.url");
+
+    }, "odontoprev.service.base");
+
+    $("#baixarContrato").click(function () {
+
+        swal({
+            title: "Aguarde",
+            text: 'Estamos baixando o contrato',
+            content: "input",
+            imageUrl: "img/load.gif",
+            showCancelButton: false,
+            showConfirmButton: false,
+            icon: "info",
+            button: {
+                text: "...",
+                closeModal: false,
+            },
+        });
+
+        callTokenVendas(function (dataToken) {
+
+            if (dataToken.status != undefined) {
+                swal("Ops!", "Algo deu errado no download, por favor tente novamente.", "error");
+                return;
+            }
+
+            var cdEmpresa = getUrlParameter("cdEmpresa");
+
+            downloadContratoPdf(function (dataArquivo) {
+
+                if (dataArquivo == undefined) {
+
+                    swal("Ops!", "Algo deu errado no download, por favor tente novamente.", "error");
+                    return;
+
+                }
+
+                if (dataArquivo.status != undefined) {
+
+                    swal("Ops!", "Algo deu errado no download, por favor tente novamente.", "error");
+                    return;
+
+                }
+
+                var link = document.createElement('a');
+                link.href = 'data:application/pdf;base64,' + dataArquivo.arquivoBase64;
+                link.download = dataArquivo.nomeArquivo;
+                link.dispatchEvent(new MouseEvent('click'));
+
+                swal.close();
+
+            }, dataToken.access_token, cdEmpresa);
+
+        });
+
+    });
+
+
+
+
+
 });
 
-/** TODO: Corrigir chamada AJAX, download Contrato PDF **/
-function downloadContrato() {
-
-    var cdEmpresa = getUrlParameter("cdEmpresa");
+function getPropertie(callback, key){
 
     $.ajax({
         async: true,
-        url: "/downloadContratacao?cdEmpresa=" + cdEmpresa,
-        //url: URLBase + "/corretorservicos/1.0/devicetoken/forcavenda/" + cdForcaVenda + "?token=" + tokenDeviceFirebase,
+        url: "/propertie?key=" + key,
         method: "GET",
         success: function (resp) {
+            console.log(resp[key]);
+            callback(resp[key]);
         },
         error: function (xhr) {
+            callback(xhr);
+        }
+    });
+
+}
+
+function downloadContratoPdf(callback, token, cdEmpresa) {
+
+    $.ajax({
+        async: true,
+        url: URLBase + contexto + metodo.replace("{CDEMPRESA}", cdEmpresa), //  "/arquivocontratacao/empresa/" + cdEmpresa + "/json",
+        method: "GET",
+        headers: {
+            "Authorization": "Bearer " + token,
+            "Content-Type": "application/json"
+        },
+        success: function (resp) {
+            callback(resp);
+        },
+        error: function (xhr) {
+            callback(xhr);
         }
     });
 
