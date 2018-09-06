63a32028a5bbaca1895aa74a20c15ed5882388ed
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-09-05 21:05:25 -0300

    COR-695 - novo dto retorno salvar email corretora (esert)

--- src/main/java/br/com/odontoprev/portalcorretor/service/CorretoraService.java
+++ src/main/java/br/com/odontoprev/portalcorretor/service/CorretoraService.java
@@ -8,47 +8,94 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import br.com.odontoprev.portalcorretor.service.dto.Corretora;
+import br.com.odontoprev.portalcorretor.service.dto.CorretoraResponse;
 
 @Service
 public class CorretoraService {
-	
-	 private static final Log log = LogFactory.getLog(CorretoraService.class);
 
-	 @Value("${odontoprev.servicebase.url}")
-	 private String requesBasetUrl;
-	 
-	 @Value("${odontoprev.corretoras.DadosCorretora}")
-	 private String dadosCorretora;
-	 
-	 @Autowired
-	 private ApiManagerTokenService apiManagerTokenService;
-	 public Corretora ObterDadosCorretora(String cnpj) {
-		 
-		 String url = requesBasetUrl + dadosCorretora + cnpj;
-	     RestTemplate restTemplate = new RestTemplate();
-		 
-	     try {
-	            HttpHeaders headers = new HttpHeaders();
-	            headers.set("Authorization", "Bearer " + apiManagerTokenService.getToken());
-	            HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-	            ResponseEntity<Corretora> retorno = restTemplate.exchange(url, HttpMethod.GET, entity, Corretora.class);
+	private static final Log log = LogFactory.getLog(CorretoraService.class);
 
-	            if (retorno.getStatusCode() == HttpStatus.OK) {
-	                return retorno.getBody();
-	            } else {
-	                return new Corretora();
-	            }
+	@Value("${odontoprev.servicebase.url}")
+	private String requesBasetUrl;
 
-	        } catch (Exception e) {
-	            //e.printStackTrace();
-	            log.error(e); 
-	            return new Corretora();
-	        }				 
-	 }
-	 
+	@Value("${odontoprev.corretoras.DadosCorretora}")
+	private String dadosCorretora;
+
+	@Value("${odontoprev.corretoras.salvarEmailCorretora}")
+	private String salvarEmailCorretora;
+
+	@Autowired
+	private ApiManagerTokenService apiManagerTokenService;
+
+	public Corretora obterDadosCorretora(String cnpj) {
+
+		String url = requesBasetUrl + dadosCorretora + cnpj;
+		RestTemplate restTemplate = new RestTemplate();
+
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.set("Authorization", "Bearer " + apiManagerTokenService.getToken());
+			HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+			ResponseEntity<Corretora> retorno = restTemplate.exchange(url, HttpMethod.GET, entity, Corretora.class);
+
+			if (retorno.getStatusCode() == HttpStatus.OK) {
+				return retorno.getBody();
+			} else {
+				return new Corretora();
+			}
+
+		} catch (Exception e) {
+			// e.printStackTrace();
+			log.error(e);
+			return new Corretora();
+		}
+	}
+
+	public CorretoraResponse salvarEmailCorretora(Corretora corretora) {
+
+	 	log.info("salvarEmailCorretora - ini");
+	 	log.info(corretora);
+	 	
+		String url = requesBasetUrl + salvarEmailCorretora;
+		RestTemplate restTemplate = new RestTemplate();
+
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.set("Authorization", "Bearer " + apiManagerTokenService.getToken());
+            headers.setContentType(MediaType.APPLICATION_JSON);
+			
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setSerializationInclusion(JsonInclude.Include.USE_DEFAULTS);
+            String stringJson = mapper.writeValueAsString(corretora);
+            
+			HttpEntity<String> entity = new HttpEntity<String>(stringJson, headers);
+			
+			ResponseEntity<CorretoraResponse> retorno = restTemplate.exchange(url, HttpMethod.PUT, entity, CorretoraResponse.class);
+
+			if (retorno.getStatusCode() == HttpStatus.OK) {
+			 	log.info("salvarEmailCorretora - fim ok");
+				return retorno.getBody();
+			} else {
+			 	log.info("salvarEmailCorretora - fim nao-ok");
+			 	log.info("retorno.getStatusCode():[" + retorno.getStatusCode() + "]");
+				return null;
+			}
+
+		} catch (Exception e) {
+			// e.printStackTrace();
+		 	log.info("salvarEmailCorretora - erro");
+			log.error(e);
+			return null;
+		}
+	}
+
 }
---
+++ src/main/java/br/com/odontoprev/portalcorretor/service/dto/CorretoraResponse.java
@@ -0,0 +1,41 @@
+package br.com.odontoprev.portalcorretor.service.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+//201809051827 - esert - COR-695 - novo dto para retorno service para salvar email corretora 
+public class CorretoraResponse implements Serializable {
+
+	private static final long serialVersionUID = 2127819694346550728L;
+
+	private long id;
+	private String mensagem;
+
+	public CorretoraResponse(long id, String mensagem) {
+		this.id = id;
+		this.mensagem = mensagem;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (!(o instanceof CorretoraResponse)) return false;
+		CorretoraResponse that = (CorretoraResponse) o;
+		return getId() == that.getId() &&
+				Objects.equals(getMensagem(), that.getMensagem());
+	}
+
+	@Override
+	public int hashCode() {
+
+		return Objects.hash(getId(), getMensagem());
+	}
+}
