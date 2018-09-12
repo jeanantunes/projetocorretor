2be61d077374fea9fca8de62b205a76fa3ae8805
Jean Antunes <jean.antunes@gmail.com>
2018-09-12 15:10:57 -0300

    COR-715 ModelAndView MEUS DADOS

--- src/main/java/br/com/odontoprev/portalcorretor/controller/ContratoCorretoraController.java
+++ src/main/java/br/com/odontoprev/portalcorretor/controller/ContratoCorretoraController.java
@@ -1,7 +1,6 @@
 package br.com.odontoprev.portalcorretor.controller;
 
-import br.com.odontoprev.portalcorretor.model.Cadastro;
-import br.com.odontoprev.portalcorretor.model.ContratoCorretora;
+import br.com.odontoprev.portalcorretor.exceptions.ApiTokenException;
 import br.com.odontoprev.portalcorretor.model.DadosContratoCorretora;
 import br.com.odontoprev.portalcorretor.model.UsuarioSession;
 import br.com.odontoprev.portalcorretor.service.ContratoCorretoraService;
@@ -10,22 +9,23 @@ import br.com.odontoprev.portalcorretor.service.dto.ContratoCorretoraPreenchido;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 @Controller
 @RestController
 public class ContratoCorretoraController {
+
+    @Autowired
+    CorretoraService corretoraService;
 
     private static final Log log = LogFactory.getLog(CorretoraController.class);
 
@@ -33,7 +33,7 @@ public class ContratoCorretoraController {
     ContratoCorretoraService contratoCorretoraService;
 
     @RequestMapping(value = "/corretora/contrato", method = RequestMethod.GET)
-    public ModelAndView home(HttpSession session){
+    public ModelAndView home(HttpSession session) {
 
         UsuarioSession usuario = (UsuarioSession) session.getAttribute("usuario");
         String codSusep = (String) session.getAttribute("codSusep");
@@ -41,9 +41,9 @@ public class ContratoCorretoraController {
 
         DadosContratoCorretora dadosContratoCorretora = new DadosContratoCorretora();
         dadosContratoCorretora.setCodSusep(codSusep);
-        dadosContratoCorretora.setCdCorretora( (long) usuario.getCodigoCorretora());
+        dadosContratoCorretora.setCdCorretora((long) usuario.getCodigoCorretora());
 
-        if (usuario.getDtAceiteContrato() != null){
+        if (usuario.getDtAceiteContrato() != null) {
 
             contratoAceito = true;
             dadosContratoCorretora.setContratoAceito(contratoAceito);
@@ -57,6 +57,32 @@ public class ContratoCorretoraController {
         dadosContratoCorretora.setContratoAceito(contratoAceito);
         dadosContratoCorretora.setTextoContrato(contratoCorretoraPreenchido.getContratoPreenchido());
         return new ModelAndView("/corretora/contrato", "contratoAceito", dadosContratoCorretora);
+    }
+
+    @RequestMapping(value = "/downloadContratoCorretora", method = RequestMethod.GET)
+    @ResponseBody
+    public ModelAndView downloadContratoCorretora(Model model, HttpServletResponse
+            response, @RequestParam("cdCorretora") Long cdCorretora) throws IOException, ApiTokenException {
+
+        ResponseEntity<String> file = corretoraService.gerarContratoCorretora(cdCorretora);
+
+        if (file == null) {
+            downloadContratoCorretora(model, response, cdCorretora);
+            model.addAttribute("error", "O arquivo não foi encontrado em nosso sistema.");
+            return new ModelAndView("meus_dados", "meus_dados", file);
+        } else {
+            String fileName = file.getHeaders().get("Content-Disposition").get(0).split(";")[1].split("=")[1];
+
+            response.setContentType(String.valueOf(MediaType.APPLICATION_PDF));
+            String headerKey = "Content-Disposition";
+            String headerValue = String.format("attachment; filename=\"%s\"", fileName);
+            response.setHeader(headerKey, headerValue);
+            response.getWriter().write(file.toString());
+            response.getWriter().flush();
+        }
+
+        downloadContratoCorretora(model, response, cdCorretora);
+        return new ModelAndView("meus_dados", "meus_dados", file);
     }
 
 }
