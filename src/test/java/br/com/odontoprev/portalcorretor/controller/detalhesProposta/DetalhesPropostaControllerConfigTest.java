fb0c9a0c130ae50c570d74957d5ed50f6261a2c4
ROBERTO <roberto.msilva@odontoprev.com.br>
2018-09-28 18:01:56 -0300

    COR-847 : DetalhesPropostaControllerConfigTest.java (esert)

---
+++ src/test/java/br/com/odontoprev/portalcorretor/controller/detalhesProposta/DetalhesPropostaControllerConfigTest.java
@@ -0,0 +1,22 @@
+package br.com.odontoprev.portalcorretor.controller.detalhesProposta;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import br.com.odontoprev.portalcorretor.controller.DetalhesPropostaController;
+
+//201809281757 - esert - COR-847 : WEB - ConfigTest para DetalhesPropostaControllerTest.java (
+@Configuration
+@ComponentScan(
+	    value = "br.com.odontoprev.portalcorretor.controller",	    		
+	    useDefaultFilters = false,
+	    includeFilters = {
+	        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DetalhesPropostaController.class)
+	    })
+
+@EnableWebMvc
+public class DetalhesPropostaControllerConfigTest {
+
+}
